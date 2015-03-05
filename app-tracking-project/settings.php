595f17613b331bdd1cd08c65970e3d4c2c83bf91
Jason Majors <jasonrmajors@gmail.com>
2015-03-04 20:32:53 -0800

    Update settings.php

--- app-tracking-project/settings.php
+++ app-tracking-project/settings.php
@@ -1,6 +1,6 @@
 <?php
 
 $PATH = "/app-tracking-project";
-define("USERNAME", "jasonmaj_atsuser");
-define("PASSWORD", "3Nb*jRo@DX5db");
+define("USERNAME", "username");
+define("PASSWORD", "password");
 ?>
